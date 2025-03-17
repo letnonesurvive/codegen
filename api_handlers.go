41f5275e6f05e14711361a1059d03bd6a6a7c6f0
letnonesurvive <hippiman1221@gmail.com>
2025-03-18 00:36:59 +0300

    something

--- api_handlers.go
+++ api_handlers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 	"strconv"
 )
@@ -9,7 +10,12 @@ func (m *MyApi) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path == "/user/profile" { // на сколько правильная идея делать сравнение именно так
 		var param ProfileParams
 		param.Login = r.URL.Query().Get("profile")
-		m.Profile(r.Context(), param)
+		user, err := m.Profile(r.Context(), param) // нужен валидатор параметров для user
+		if err != nil && err.Error() == "bad user" {
+			http.Error(w, err.Error(), 500)
+		}
+		data, _ := json.Marshal(user)
+		w.Write(data)
 	} else if r.URL.Path == "/user/create/" {
 		age, _ := strconv.Atoi(r.URL.Query().Get("age"))
 		param := CreateParams{
